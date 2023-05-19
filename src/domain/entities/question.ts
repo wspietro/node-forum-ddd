633cf0067056d66fe2c9b9667a8cb4fdb5f1936c
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-05-19 12:55:38 -0300

    :sparkles: feat: superclasse entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
+import { Entity } from "../../core/entities/entity"
 
 interface QuestionProps {
   title: string
@@ -8,18 +8,6 @@ interface QuestionProps {
   authorId: string
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public content: string
-  public slug: Slug
-  public authorId: string
+export class Question extends Entity<QuestionProps>{
 
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title
-    this.content = props.content
-    this.slug = props.slug
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
 }
