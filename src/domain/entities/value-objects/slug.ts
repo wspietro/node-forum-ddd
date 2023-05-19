cea0be695c0aea83cc4f67e6379b8dbce7d7a28f
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-05-19 12:17:52 -0300

    :sparkles: feat: slug value object

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,26 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and convert it as a slug.
+   * 
+   * Example: "An example title" => "an-example-title"
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+}
