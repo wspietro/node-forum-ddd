91abbe001f868d12dfbf88d2b37ddde4f8abc03b
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-05-22 22:43:29 -0300

    :recycle: refactor: geracao id por classe

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,7 +1,8 @@
 import { randomUUID } from "node:crypto"
+import { UniqueEntityId } from "./unique-entity-id"
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityId
   protected props: Props
 
   get id() {
@@ -10,6 +11,6 @@ export class Entity<Props> {
 
   constructor(props: Props, id?: string) {
     this.props = props
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityId(id)
   }
 }
