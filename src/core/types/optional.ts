8beb618ca20114cc1283e56a038b392a005cdc69
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-05-23 21:27:58 -0300

    :sparkles: feat: criando utiliy typescript

---
+++ src/core/types/optional.ts
@@ -0,0 +1,21 @@
+/**
+ * Make some property optional on type
+ *
+ * @example
+ * ```typescript
+ * type Post {
+ *  id: string;
+ *  name: string;
+ *  email: string;
+ * }
+ *
+ * Optional<Post, 'id' | 'email'>
+ * ```
+ * &: Represents the intersection of types.It combines the
+ * properties from Pick<Partial<T>, K> (optional
+ * properties) with the properties from Omit<T, K> (non-
+ * optional properties).
+ * 
+ **/
+
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
