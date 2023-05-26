4c07673f6d83a87b2101f2eb0c7e1216ac073b74
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-05-25 22:36:03 -0300

    :sparkles: feat: path aliases e vitest global

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,7 @@
 import { Slug } from "./value-objects/slug"
-import { Entity } from "../../core/entities/entity"
-import { UniqueEntityId } from "../../core/entities/unique-entity-id"
-import { Optional } from "../../core/types/optional"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityId } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
 import dayjs from "dayjs"
 
 interface QuestionProps {
