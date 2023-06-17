b8383733376ee451a9d35ef358dafe98ed13af0e
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-06-17 11:40:01 -0300

    :sparkless: feat: faker dados ficticios

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -18,6 +18,8 @@ describe('Get Question By Slug', () => {
       slug: Slug.create('example-question'),
     })
 
+    console.log(newQuestion)
+
     await inMemoryQuestionsRepository.create(newQuestion)
 
     const { question } = await sut.execute({
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example Question',
-    authorId: new UniqueEntityId(),
-    slug: Slug.create('example-question'),
-    content: 'content example question',
-    ...override, // spread: In a situation where one key has another property, the last property overrides the first instance.
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override, // spread: In a situation where one key has another property, the last property overrides the first instance.
+    },
+    id,
+  )
 
   return question
 }
