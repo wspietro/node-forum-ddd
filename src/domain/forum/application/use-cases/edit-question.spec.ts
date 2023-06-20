90d33b610bb0c552943de2c8a23e450bc4c82102
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-06-19 21:47:03 -0300

    :sparkles: feat: edit-question use-case e teste

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-reposiroty'
+import { EditQuestionUseCase } from './edit-question'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+// Automatizando processo de criacao do repositorio e do use-case
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(async () => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  test('it should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toValue(),
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteudo teste',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Pergunta teste',
+      content: 'Conteudo teste',
+    })
+  })
+
+  test('it should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestion.id.toValue(),
+        authorId: 'author-2',
+        title: 'Pergunta teste',
+        content: 'Conteudo teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
