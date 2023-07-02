63e577c2bd813959e517db001d817dd7a3c55be9
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-07-02 14:58:04 -0300

    :sparkles: feat: question-comments use-case

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private questinCommentRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityId(authorId),
+      questionId: new UniqueEntityId(questionId),
+      content,
+    })
+
+    await this.questinCommentRepository.create(questionComment)
+
+    return { questionComment }
+  }
+}
