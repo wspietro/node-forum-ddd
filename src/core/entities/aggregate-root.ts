82ac89b381d49726376fd4433ec60b68df8fdfea
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-10-05 13:29:18 -0300

    :construction: feat: domain events structure

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,5 +1,20 @@
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domains-events'
 import { Entity } from './entity'
 
 export abstract class AggregateRoot<Props> extends Entity<Props> {
-  // Por hora, apenas simboliza o root
+  private _domainEvent: DomainEvent[] = []
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvent
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvent.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvent = []
+  }
 }
--- src/domain/forum/enterprise/entities/answer-attachment-list.ts
+++ src/domain/forum/enterprise/entities/answer-attachment-list.ts
@@ -1,8 +1,8 @@
 import { WatchedList } from '@/core/entities/watched-list'
-import { AnswerAttachment } from './question-attachment'
+import { AnswerAttachment } from './answer-attachment'
 
 export class AnswerAttachmentList extends WatchedList<AnswerAttachment> {
   compareItems(a: AnswerAttachment, b: AnswerAttachment): boolean {
-    return a.attachmentId === b.attachmentId
+    return a.attachmentId.equals(b.attachmentId)
   }
 }
