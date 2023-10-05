d9b2edb0e17c3d437aa9b4af89a3a3d9f206130f
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-10-05 12:16:43 -0300

    :sparkles: feat: read notification

---
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { ReadNotificationUseCase } from './read-notification'
+import { makeNotification } from 'test/factories/make-notification'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: ReadNotificationUseCase
+
+describe('Read Notification', () => {
+  beforeEach(async () => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sut = new ReadNotificationUseCase(inMemoryNotificationsRepository)
+  })
+
+  test('it should be able to read a notification', async () => {
+    const notification = makeNotification()
+
+    await inMemoryNotificationsRepository.create(notification)
+
+    const result = await sut.execute({
+      recipientId: notification.recipientId.toString(),
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityId('recipient-1'),
+    })
+
+    await inMemoryNotificationsRepository.create(notification)
+
+    const result = await sut.execute({
+      notificationId: notification.id.toString(),
+      recipientId: 'recipient-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -2,7 +2,7 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: UniqueEntityId
   title: string
   content: string
@@ -29,6 +29,10 @@ export class Notification extends Entity<NotificationProps> {
 
   get createdAt() {
     return this.props.createdAt
+  }
+
+  read() {
+    this.props.readAt = new Date()
   }
 
   static create(
