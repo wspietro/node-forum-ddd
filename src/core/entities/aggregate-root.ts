8dcb598dd439b348182412dae4f8598ef4b39760
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-10-06 11:18:51 -0300

    :sparkles: feat: answer created event

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,5 +1,5 @@
 import { DomainEvent } from '../events/domain-event'
-import { DomainEvents } from '../events/domains-events'
+import { DomainEvents } from '../events/domain-events'
 import { Entity } from './entity'
 
 export abstract class AggregateRoot<Props> extends Entity<Props> {
